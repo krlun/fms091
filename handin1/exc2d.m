0c68b99d57551753dae0de27eae2ccc94ba66558
Kristoffer Lundgren <krlun@tyler2.local>
2017-02-06 16:59:31 +0100

    ny fil

--- handin1/exc2d.m
+++ handin1/exc2d.m
@@ -2,6 +2,7 @@ function [P] = exc2d(k, lambda)
 %EXC2D calculates the probability (explicitly) that a
 %Nordex N90-2500 LS wind turbine produces power given that the wind is
 %modelled using a Weibull distribution.
+
     P = wblcdf(25, lambda, k) - wblcdf(3.5, lambda, k)
 
 end
---
+++ handin1/exc3b.m
@@ -0,0 +1,8 @@
+function [output_args] = exc3b(input_args)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+%
+
+
+end
+
