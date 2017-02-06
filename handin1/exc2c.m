3355326d9bca02fef2c3cff37bf4d0f1a45a7bcf
Kristoffer Lundgren <krlun@tyler2.local>
2017-02-06 15:40:25 +0100

    .

---
+++ handin1/exc2c.m
@@ -0,0 +1,19 @@
+function [IAS] = exc2c(N, k, lambda, lambdap)
+%EXC2C uses antithetic sampling to reduce the variance in calculating
+%the a confidence interval for the estimated power output of a
+%Nordex N90-2500 LS wind turbine, where the wind is modelled using a
+%Weibull distribution.
+
+    load('powercurve_N90.mat');
+
+    X = rand(1, N);
+    Y = 1 - X;
+    MC1 = P(wblinv(X, lambda, k));
+    MC2 = P(wblinv(Y, lambda, k));
+    W = (MC1 + MC2)/2;
+    tauAS = sum(W/N);
+    sigma2AS = var(W);
+    IAS = [tauAS - lambdap*sqrt(sigma2AS/N), tauAS, ...
+        tauAS + lambdap*sqrt(sigma2AS/N)];
+    
+end
