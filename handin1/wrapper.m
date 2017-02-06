c7086151a3c5d2e6f6c315a4f724cf9247a4780f
Kristoffer Lundgren <kristoffer.lundgren@gmail.com>
2017-02-06 18:27:24 +0100

    .

--- handin1/wrapper.m
+++ handin1/wrapper.m
@@ -1,8 +1,6 @@
 rho = 1.225; % kg/m^3
 k = 1.7; lambda = 9.2; % Weibull parameters
 lambdap = norminv(0.975); % 95% confidence interval
-B = k;
-A = lambda;
 N = 10000; % # of samples for MC simulation
 d = 90; % diameter of rotors
 
