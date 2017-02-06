56991c1fd809ab06dc44b5a2caf542430429a553
Kristoffer Lundgren <krlun@tyler2.local>
2017-02-06 18:21:43 +0100

    .

---
+++ handin1/exc2f.m
@@ -0,0 +1,9 @@
+function [capfact, availfact] = exc2f(N, k, lambda)
+%EXC2F Summary of this function goes here
+%   Detailed explanation goes here
+
+    MCest = P(wblrnd(lambda, k, 1, N));
+    capfact = sum(MCest)/(N*2.5*10^6); %Bra
+    availfact = wblcdf(25, lambda, k) - wblcdf(3.5, lambda, k); %Inte s? bra
+
+end
