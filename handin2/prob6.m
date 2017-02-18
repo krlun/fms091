a62c3c9002e2cb5dd4b53082ab04aab10629791d
Kristoffer Lundgren <kristoffer.lundgren@gmail.com>
2017-02-18 16:25:15 +0100

    .

--- handin2/prob6.m
+++ handin2/prob6.m
@@ -1,7 +1,23 @@
-function [ output_args ] = prob6( input_args )
+function [A, mu, gamma] = prob6(N, nStop)
 %PROB6 Summary of this function goes here
 %   Detailed explanation goes here
 
+    %ln(c_n(2)) + ln(n) = ln(A_2) + n*ln(mu_d) + gamma*logn
+    vec = (1:nStop)';
+    cns = []; %Values of c_2(n)
+    N = 10000;
+    for i = vec'
+        cns = [cns; prob5(N, i)];
+    end
+    y = log(cns) + log(vec); %ln(cn(2))+ln(n)
+    X = zeros(nStop, 3);
+    X(:, 1) = ones(nStop, 1);
+    X(:, 2) = vec;
+    X(:, 3) = log(vec);
+    B = (X'*X)\X'*y; %B(1) = log(A), B(2) = log(mu), B(3) = gamma.
+    A = exp(B(1));
+    mu = exp(B(2));
+    gamma = B(3);
 
 end
 
--- handin2/proj2.m
+++ handin2/proj2.m
@@ -1,10 +1,12 @@
-N = 10000; n = 7; d = 2;
+N = 10000; n = 7; d = 2; nStop = 10;
 
 [cn] = prob3(N, n);
 
 [cn] = prob4(N, n);
 
 [cn] = prob5(N, n);
+
+[A, mu, gamma] = prob6(N, nStop);
 
 
 [cn] = prob9(N, n, d);
