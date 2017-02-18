b8afcf6035b31a7071cead6aea4d2a6595f9da7c
Kristoffer Lundgren <kristoffer.lundgren@gmail.com>
2017-02-18 15:55:53 +0100

    .

--- handin2/prob9.m
+++ handin2/prob9.m
@@ -1,7 +1,58 @@
-function [ output_args ] = prob9( input_args )
+function [cn] = prob9(N, n, d)
 %PROB9 Summary of this function goes here
 %   Detailed explanation goes here
 
+    %d = 4;
+    %n = 7;
+    %N = 200;
+    currentWeights = ones(1, N); %De nuvarande vikterna!
+    coords = zeros(N, n+1, d); %coords(sampel i, efter n steg, dimension d)
+    cn = 1;
+    for s = 1:n
+        newCoords = zeros(N, s, d);
+        probs = cumsum(currentWeights/sum(currentWeights));
+        for i = 1:N
+            r = rand;
+            index = 1;
+            for k = 1:N
+                if (r > probs(k))
+                    index = k;
+                else
+                    break;
+                end
+            end
+            newCoords(i, :, :) = coords(k, 1:s, :);
+        end
+        coords(:, 1:s, :) = newCoords; %Kordinaterna blir de nya
+        currentWeights = ones(1, N); %Nollst?ller vikterna
+        newCoords = zeros(N, d); %De nya kordinaterna nollst?lls
+        for i = 1:N
+            currentPos = squeeze(coords(i, s, :));
+            possibleNext = [];
+            for j = 1:(2*d)
+                possibleNext = [possibleNext currentPos];
+            end
+            %possibleNext = repmat(currentPos, 1, 2*d);
+            for dim = 1:d
+                possibleNext(dim, 2*dim-1) = possibleNext(dim, 2*dim-1) + 1;
+                possibleNext(dim, 2*dim) = possibleNext(dim, 2*dim) - 1;
+            end     
+            freespots = [];
+            for j = 1:(2*d)
+                currCords = (squeeze(coords(i,:,:)));
+                check = ismember(currCords, possibleNext(:, j)', 'rows');
+                if ~any(check)
+                    freespots = [freespots possibleNext(:, j)];
+                end
+            end
+            if ~isempty(freespots)
+                r = ceil(rand * length(freespots(1, :)));
+                newCoords(i, :) = freespots(:, r);
+            end
+            currentWeights(i) = length(freespots(1,:));
+        end
+        coords(:,s+1,:) = newCoords;
+        cn = cn*sum(currentWeights)/N;
+    end
 
 end
-
