e6dd13a139ca7e6d8c3ee191b1f6e389ffad18c0
Kristoffer Lundgren <kristoffer.lundgren@gmail.com>
2017-02-18 15:44:11 +0100

    .

---
+++ handin2/test.m
@@ -0,0 +1,16 @@
+nrOfSamples = 100;
+N = 5000;
+n = 7;
+lambdap = norminv(0.975);
+
+samples = zeros(1, nrOfSamples);
+
+
+for i = 1:nrOfSamples
+    i
+    samples(i) = prob5(N, n);
+end
+
+mu = sum(samples)/nrOfSamples;
+v = var(samples);
+I = [mu - lambdap*sqrt(v/nrOfSamples), mu, mu + lambdap*sqrt(v/nrOfSamples)];
