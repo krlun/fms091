f01e11e49701a37675500da33c29871750ef79a2
Kristoffer Lundgren <kristoffer.lundgren@gmail.com>
2017-02-20 15:42:23 +0100

    .

--- handin2/proj2.m
+++ handin2/proj2.m
@@ -53,18 +53,26 @@ for n = nStart:nStop
     I(n, :) = [mu - lambdap*sqrt(v/nrOfSamples), mu, mu + lambdap*sqrt(v/nrOfSamples)];
 end
 
-%%
+%% Problem 9
+N = 1000;
+d = 9;
+nrOfSamples = 100;
+lambdap = norminv(0.975);
+nStart = 1; nStop = 8;
 
-samples = zeros(1, nrOfSamples);
-parfor i = 1:nrOfSamples
-    i
-    samples(i) = prob9(N, n);
+samples = zeros(nStop-nStart + 1, nrOfSamples);
+I = zeros(nStop-nStart + 1, 3);
+
+for n = nStart:nStop
+    for j = 1:nrOfSamples
+        samples(n, j) = prob9(N, n, d);
+    end
+    mu = sum(samples(n, :))/nrOfSamples;
+    v = var(samples(n, :));
+    I(n, :) = [mu - lambdap*sqrt(v/nrOfSamples), mu, mu + lambdap*sqrt(v/nrOfSamples)];
 end
 
-mu = sum(samples)/nrOfSamples;
-v = var(samples);
-I = [mu - lambdap*sqrt(v/nrOfSamples), mu, mu + lambdap*sqrt(v/nrOfSamples)];
-
+%%
 
 %[cn] = prob3(N, n);
 
