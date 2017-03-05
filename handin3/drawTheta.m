5d58201ed4b86e2b2c9e95c00adaf7c292b348fd
Kristoffer Lundgren <kristoffer.lundgren@gmail.com>
2017-03-05 23:18:52 +0100

    .

--- handin3/drawTheta.m
+++
@@ -1,7 +0,0 @@
-function [theta] = drawTheta(lambda, psi)
-%DRAWTHETA Summary of this function goes here
-%   Detailed explanation goes here
-    d = length(lambda);
-    theta = gamrnd(2*d + 2, 1/(psi + sum(lambda)));
-end
-
