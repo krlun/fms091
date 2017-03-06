a9c50acb25175768dc28d2d377dafa93c84fc34c
tennieskil <tennieskil.tennis@gmail.com>
2017-03-06 16:27:59 +0100

    Hela del 2 fixad

--- handin3/exc2/proj32.m
+++ handin3/exc2/proj32.m
@@ -1,5 +1,4 @@
 % Task b)
-clear all;
 load('atlantic.txt');
 [beta, mu] = est_gumbel(atlantic);
 
@@ -24,14 +23,24 @@ betaSort = sort(betas - beta);
 muSort = sort(mus - mu);
 alpha = 0.05;
 
-betaLB = beta - betaSort(ceil((1-alpha/2)*samples));
-betaUB = beta - betaSort(ceil(alpha/2*samples));
+betaLB = 2*beta - mean(beta) - betaSort(ceil((1-alpha/2)*samples)); %Biased-corrected estimate
+betaUB = 2*beta - mean(beta) - betaSort(ceil(alpha/2*samples)); %Biased-corrected estimate
 betaI = [betaLB betaUB]
 
-muLB = mu - muSort(ceil((1-alpha/2)*samples));
-muUB = mu - muSort(ceil(alpha/2*samples));
+muLB = 2*mu - mean(mu) - muSort(ceil((1-alpha/2)*samples)); %Biased-corrected estimate
+muUB = 2*mu - mean(mu) - muSort(ceil(alpha/2*samples)); %Biased-corrected estimate
 muI = [muLB muUB]
-
+%% Plots of task 2b
+histogram(betas,100)
+title('Distribution of 10000 parametric bootstrapped estimates of \beta')
+xlabel('Value of \beta')
+ylabel('Number of occasions for the value')
+figure
+histogram(mus,100)
+title('Distribution of 10000 parametric bootstrapped estimates of \mu')
+xlabel('Value of \mu')
+ylabel('Number of occasions for the value')
+%%
 % Task c), use the betas and mus from b)
 T = 3*14*100;
 returnValues = zeros(1, samples);
@@ -39,11 +48,14 @@ for i = 1:samples
     returnValues(i) = F_inv(1-1/T, betas(i), mus(i));
 end
 
-m = F_inv(1-1/T, beta, mu);
-
+meanRV = F_inv(1-1/T, beta, mu);
 meanReturnValue = mean(returnValues);
-returnValueSort = sort(returnValues - m);
-returnValueLB = m - returnValueSort(ceil((1-alpha)*samples));
-returnValueUB = m - returnValueSort(ceil((alpha)*samples));
+returnValueSort = sort(returnValues - meanReturnValue);
+returnValueLB = 2*meanRV - meanReturnValue - returnValueSort(ceil((1-alpha)*samples)); %Biased-corrected estimate
+returnValueUB = 2*meanRV - meanReturnValue - returnValueSort(ceil((alpha)*samples)); %Biased-corrected estimate
 returnValueI = [returnValueLB returnValueUB]
-
+%% Plots of task 2c
+histogram(returnValues,100)
+title('Distribution of 10000 parametric bootstrapped estimates of the 100-year return value')
+xlabel('Estimated return value')
+ylabel('Number of occasions for the value')
